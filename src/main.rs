c46a98f336d63a53f16751ac8821fb3f105913ad
Mingyi BAO <bmyjacks@outlook.com>
2025-02-13 14:42:43 +0800

    Add Clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+/// A funny Micro language compiler
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Sets input Micro source file
+    #[arg(value_name = "INPUT")]
+    source_file: PathBuf,
+
+    /// Sets output LLVM IR file
+    #[arg(short, value_name = "OUTPUT", default_value = "a.ll")]
+    output_file: Option<PathBuf>,
+
+    /// Use verbose output
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Args::parse();
 }
